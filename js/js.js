bf6cab150543aca5b38c19ecc4dc460ba514fd4c
KitsuneVog <86886280+KitsuneVog@users.noreply.github.com>
2024-01-09 20:20:56 +0300

    /

--- js/js.js
+++ js/js.js
@@ -1,14 +1,22 @@
-var boxshadow = "";
+function showStars () {
+    var boxshadow = "";
 
-for(var i = 0; i <= 2000; i++){
-    px = Math.random() < 0.5 ? "-" : "";
-    py = Math.random() < 0.5 ? "-" : "";
-    x = Math.floor((Math.random() * window.innerWidth) + 1);
-    y = Math.floor((Math.random() * window.innerHeight) + 1);
-    s = Math.floor((Math.random() * 2) - 1);
-    boxshadow += px+x+"px "+py+y+"px 0 "+s+"px #fff,";
+    for(var i = 0; i <= window.innerWidth; i++){
+        px = Math.random() < 0.5 ? "-" : "";
+        py = Math.random() < 0.5 ? "-" : "";
+        x = Math.floor((Math.random() * window.innerWidth) + 1);
+        y = Math.floor((Math.random() * window.innerHeight) + 1);
+        s = Math.floor((Math.random() * 2) - 1);
+        boxshadow += px+x+"px "+py+y+"px 0 "+s+"px #fff,";
+    }
+
+    boxshadow = boxshadow.slice(0, -1);
+
+    stars.style.boxShadow = boxshadow;
 }
 
-boxshadow = boxshadow.slice(0, -1);
+showStars ();
 
-stars.style.boxShadow = boxshadow;
+window.addEventListener("resize", function() {
+    showStars ();
+}, false);
